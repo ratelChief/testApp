36d33a8a186a4d42d113611feff5b4603ca51ba0
Dmitry Svistun <dima1002010@live.com>
2020-06-22 00:56:14 +0300

    added store persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
-export default createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistConfig = {
+  key: 'reducer',
+  storage,
+  whitelist: ['reducer'],
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(pReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistor = persistStore(store);
+
+export { persistor, store };
